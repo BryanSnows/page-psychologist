8123098457fa2da2f1e3de4a4ec1bd13f0d02982
Bryan Neves <bryan.neves@indt.org.br>
2024-07-28 10:39:50 -0400

    fix: add carousel

--- carousel.js
+++ carousel.js
@@ -1,32 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const prevButton = document.querySelector(".carousel-prev");
-  const nextButton = document.querySelector(".carousel-next");
-  const carousel = document.querySelector(".carousel");
-  const slides = Array.from(carousel.querySelectorAll(".carousel-slide"));
-  let currentIndex = 0;
-  const intervalTime = 5000;
+  const carousels = document.querySelectorAll(".carousel");
 
-  const updateCarousel = () => {
-    const offset = -currentIndex * 100;
-    slides.forEach((slide) => {
-      slide.style.transform = `translateX(${offset}%)`;
-    });
-  };
+  carousels.forEach((carousel) => {
+    const prevButton = carousel
+      .closest(".carousel-container")
+      .querySelector(".carousel-prev");
+    const nextButton = carousel
+      .closest(".carousel-container")
+      .querySelector(".carousel-next");
+    const slides = Array.from(carousel.querySelectorAll(".carousel-slide"));
+    let currentIndex = 0;
+    const intervalTime = 5000;
 
-  const showNextSlide = () => {
-    currentIndex = currentIndex === slides.length - 1 ? 0 : currentIndex + 1;
+    const updateCarousel = () => {
+      const offset = -currentIndex * 100;
+      slides.forEach((slide) => {
+        slide.style.transform = `translateX(${offset}%)`;
+      });
+    };
+
+    const showNextSlide = () => {
+      currentIndex = currentIndex === slides.length - 1 ? 0 : currentIndex + 1;
+      updateCarousel();
+    };
+
+    const showPrevSlide = () => {
+      currentIndex = currentIndex === 0 ? slides.length - 1 : currentIndex - 1;
+      updateCarousel();
+    };
+
+    prevButton.addEventListener("click", showPrevSlide);
+    nextButton.addEventListener("click", showNextSlide);
+
+    setInterval(showNextSlide, intervalTime);
+
     updateCarousel();
-  };
-
-  const showPrevSlide = () => {
-    currentIndex = currentIndex === 0 ? slides.length - 1 : currentIndex - 1;
-    updateCarousel();
-  };
-
-  prevButton.addEventListener("click", showPrevSlide);
-  nextButton.addEventListener("click", showNextSlide);
-
-  setInterval(showNextSlide, intervalTime);
-
-  updateCarousel();
+  });
 });
